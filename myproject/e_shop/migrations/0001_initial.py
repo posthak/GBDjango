0a276113eb67172a181d91d4ff5314e952c20098
posthak <adler_m@mail.ru>
2024-02-28 13:22:40 +0100

    HW5 commited changes

--- myproject/e_shop/migrations/0001_initial.py
+++ myproject/e_shop/migrations/0001_initial.py
@@ -14,11 +14,19 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=50, unique=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Product',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='e_shop.category')),
                 ('photo', models.ImageField(blank=True, null=True, upload_to='product/')),
                 ('price', models.DecimalField(decimal_places=2, max_digits=8)),
                 ('quantity', models.IntegerField()),

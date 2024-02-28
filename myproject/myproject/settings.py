d1d37cfbb3327f9b5d957f2fd5ba5d451dd50a45
posthak <adler_m@mail.ru>
2024-02-28 14:16:58 +0100

    Commited for pythonanywhere

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,14 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7iop-qhy=2^u61$p(oddq9*h8c&(2go2-!t%)zk)vdgc@l1vu9'
+# SECRET_KEY = 'django-insecure-7iop-qhy=2^u61$p(oddq9*h8c&(2go2-!t%)zk)vdgc@l1vu9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
-    'localhost',
     '127.0.0.1',
+    'adlermichael.pythonanywhere.com'
 ]
 
 
@@ -81,13 +84,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adlermichael$default',
+        'USER': 'adlermichael',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'adlermichael.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -125,9 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
+STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_URL = 'product/'
 MEDIA_ROOT = BASE_DIR
 # Default primary key field type

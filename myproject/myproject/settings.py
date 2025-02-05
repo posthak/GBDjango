ec0bc104f24b7f46448270b9fdf6bc1a0fc6401e
posthak <adler_m@mail.ru>
2025-02-05 22:01:52 +0100

    Set up settings

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,17 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-7iop-qhy=2^u61$p(oddq9*h8c&(2go2-!t%)zk)vdgc@l1vu9'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-7iop-qhy=2^u61$p(oddq9*h8c&(2go2-!t%)zk)vdgc@l1vu9'
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-SESSION_COOKIE_SECURE = True
+# DEBUG = False
+DEBUG = True
+# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'adlermichael.pythonanywhere.com'
+    # 'adlermichael.pythonanywhere.com'
 ]
 
 
@@ -84,26 +86,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'adlermichael$default',
-        'USER': 'adlermichael',
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'adlermichael.mysql.pythonanywhere-services.com',
-        'OPTIONS': {
-            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
-    }
-}
-#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'adlermichael$default',
+#         'USER': 'adlermichael',
+#         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+#         'HOST': 'adlermichael.mysql.pythonanywhere-services.com',
+#         'OPTIONS': {
+#             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#         },
 #     }
 # }
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
